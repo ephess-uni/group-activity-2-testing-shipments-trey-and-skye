35efddc299c1885d32bbadd5cb210aee0cd2e4d8
skyedaninewm <143444435+skyedaninewm@users.noreply.github.com>
2023-09-23 11:50:45 -0500

    Group activity

--- tests/test_shipment_pairs.py
+++ tests/test_shipment_pairs.py
@@ -1,8 +1,34 @@
 # Create your pytest cases here
-from src.shipment_pairs import remove_intra, collect_pairs
 
+from src.shipment_pairs import remove_intra, collect_pairs
+import pytest
 
 # Write tests for remove_intra
+
+__input = [
+            ['AB', 'BA', 9],
+            ['AB', 'BA', 3],
+            ['BA', 'AB', 6],
+            ['AB', 'AB', 5]
+        ]
+
+__expected = [
+                ['AB', 'BA', 9],
+                ['AB', 'BA', 3],
+                ['BA', 'AB', 6]
+        ]
+@pytest.mark.parametrize(
+    'test_input, expected',
+    [
+        (__input, __expected)
+    ]
+)
+def test_remove_intra(test_input, expected):
+    assert remove_intra(test_input) == expected
+
+
+
+
 
 
 # Write tests for collect_pairs
